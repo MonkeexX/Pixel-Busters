b05c0ed249bdd5187932f05be8e324f74a31144d
MonkeexX <gerard.busca@enti.cat>
2024-02-19 13:59:33 +0100

    Modified character movement

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class characterController : MonoBehaviour
 {
+    public float movementSpeed = 5.0f;
+    public float jumpForce = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,17 @@ public class characterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpForce);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movementSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-movementSpeed, 0);
+        }
     }
 }

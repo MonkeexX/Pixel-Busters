c608511c705669deb6f45bbb2da221b1007f4ded
MonkeexX <gerard.busca@enti.cat>
2024-02-19 12:51:12 +0100

    Added character controller and Script and Texture folders

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test1 : MonoBehaviour
+public class characterController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

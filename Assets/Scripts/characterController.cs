500cc170546f1a9a8ce1c593ab265d796b5d38ae
SergiNaranjo <sergi.naranjo@enti.cat>
2024-02-19 18:23:07 +0100

    Textures

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class characterController : MonoBehaviour
 {
-<<<<<<< HEAD
-    public float movementSpeed = 5.0f;
-    public float jumpForce = 5.0f;
 
-=======
->>>>>>> c608511c705669deb6f45bbb2da221b1007f4ded
+    public float movementSpeed = 5f;
+    public float jumpForce = 300f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,21 +17,18 @@ public class characterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
+
         if (Input.GetButtonDown("Jump"))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpForce);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(movementSpeed, 0);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-movementSpeed, 0);
         }
-=======
-        
->>>>>>> c608511c705669deb6f45bbb2da221b1007f4ded
     }
 }

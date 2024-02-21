1245c89fd741adc0a8be388cff82f27fa396942d
SergiNaranjo <sergi.naranjo@enti.cat>
2024-02-21 15:16:40 +0100

    Jump done

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -4,36 +4,46 @@ using UnityEngine;
 
 public class characterController : MonoBehaviour
 {
-<<<<<<< HEAD
-    public float movementSpeed = 5.0f;
-    public float jumpForce = 5.0f;
+    public float movementSpeed = 57.0f;
+    public float jumpForce = 35.0f;
+    public int saltosMaximos = 2;
+    public int saltosRestantes;
+    public LayerMask capaSuelo;
 
-=======
->>>>>>> c608511c705669deb6f45bbb2da221b1007f4ded
     // Start is called before the first frame update
     void Start()
     {
-        
+        saltosRestantes = saltosMaximos;
     }
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-        if (Input.GetButtonDown("Jump"))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpForce);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(movementSpeed, 0);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-movementSpeed, 0);
-        }
-=======
         
->>>>>>> c608511c705669deb6f45bbb2da221b1007f4ded
+        if (InTheFloor()) //Reset of jumps when the character touches the floor
+        {
+            saltosRestantes = saltosMaximos;      
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && saltosRestantes > 0) //Jump input
+        {
+            saltosRestantes = saltosRestantes - 1;
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        if (Input.GetKey(KeyCode.D)) //Right movement
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.right * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A)) //Left movement
+        {
+            GetComponent<Rigidbody2D>().AddForce(-Vector2.right * movementSpeed);
+        }
+        InTheFloor();
+    }
+
+    bool InTheFloor() //Function to reset the number of jumps
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(GetComponent<BoxCollider2D>().bounds.center, new Vector2(GetComponent<BoxCollider2D>().bounds.size.x, GetComponent<BoxCollider2D>().bounds.size.y), 0f, Vector2.down,
+            0.2f, capaSuelo);
+        return raycastHit.collider != null;
     }
 }

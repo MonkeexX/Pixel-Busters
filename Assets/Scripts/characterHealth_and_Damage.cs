f559290b04a45e819a36dd7211a9ff8b67c26bc4
HectorZ70 <hector.zornoza@enti.cat>
2024-03-01 10:58:11 +0100

    Damage added with errors

---
+++ Assets/Scripts/characterHealth_and_Damage.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterHealth_and_Damage : MonoBehaviour
+{
+    public int health = 100;
+    
+    public void RestHealth(int quantity)
+    {
+        health = quantity -1;
+    }
+
+  
+}

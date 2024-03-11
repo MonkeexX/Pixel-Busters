28d80c1a2a8763c8c44a85b5a20ee650bb497d54
MonkeexX <gerard.busca@enti.cat>
2024-03-11 12:28:43 +0100

    Added Box
    The box has collisions and when shoot the box is destroyed. Also you can push the box

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject bullet = GameObject.FindGameObjectWithTag("Bullet");
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
